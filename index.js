4670079665ab54f6160d4c935a4fec30900f80f5
bouquet1 <bukuleta@gmail.com>
2023-03-20 08:57:15 -0700

    complete arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,36 @@
 
-// Write your code here
+// function multiply(num1, num2) {
+//     return num1 * num2;
+// }
+// console.log(multiply(31, 2));
+
+let num1 = 31;
+let num2 = 2;
+let num3 = 9;
+let num4 = 5;
+
+const multiply = num1 * num2;
+
+const mod = num3 % num4;
+
+const max = Math.max(5,10,15,20);
+
+const random = Math.floor(Math.random() * 10) +1;
+Math.isInteger(random);
+
+
+// const mod = (num3, num4) => {
+//     return num3 % num4;
+// }
+
+// function mod(num3, num4) {
+//     return num3 % num4;
+// }
+// console.log(mod(9,5));
+
+// const mod = (num3, num4) => num3 % num4;
+
+// console.log(mod(9, 5));
+
+
+
